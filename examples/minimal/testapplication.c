7dc6be72de37d73636a25d8a244ce44ebe54023a
Frank Engelhardt <frank.engelhardt@ovgu.de>
2021-06-01 13:04:13 +0200

    add minimal example

---
+++ examples/minimal/testapplication.c
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2021 Otto-von-Guericke-Universität Magdeburg
+ */
+
+/**
+ * @defgroup
+ * @ingroup
+ * @brief
+ * @{
+ *
+ * @file
+ * @brief
+ *
+ * @author  Frank Engelhardt <fengelha@ovgu.de>
+ */
+#include <stdio.h>
+#include <doriot_dca.h>
+#include <msg.h>
+#include <shell.h>
+#include <doriot_dca/udp_throughput.h>
+#include <doriot_dca/latency.h>
+
+
+#define MAIN_QUEUE_SIZE     (8)
+static msg_t _main_msg_queue[MAIN_QUEUE_SIZE];
+
+static const shell_command_t shell_commands[] = {
+    { NULL, NULL, NULL }
+};
+
+int main(void)
+{
+    udp_server(1883);
+    msg_init_queue(_main_msg_queue, MAIN_QUEUE_SIZE);
+    char line_buf[SHELL_DEFAULT_BUFSIZE];
+
+    shell_run(shell_commands, line_buf, SHELL_DEFAULT_BUFSIZE);
+    return 0;
+}
